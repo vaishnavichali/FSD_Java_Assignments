fda86f189fd84c5a98cec17e0861610305232371
vaishnavichali <33795415+vaishnavichali@users.noreply.github.com>
2019-10-01 08:53:50 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -5,9 +5,9 @@ class Factorial{
 		System.out.println("Enter a number :");
 		java.util.Scanner scan=new java.util.Scanner(System.in);
 		input=scan.nextInt();
-		for(int i=1;i<=input;i++) 
+		for(int i=1; i<=input; i++) 
 		{
-             fact=fact*i;
+                   fact = fact*i;
 		}
 		System.out.println("Factorial of "+input+" is "+fact);
 	}
